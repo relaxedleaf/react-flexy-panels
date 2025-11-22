ce6a231db18a6316ea86b3db86a34a04138bc9a4
Guanghui Li <liguanghui.cs@gmail.com>
2025-11-22 13:19:46 -0600

    feat: init docs app

---
+++ apps/docs/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@react-flexy-panels/docs"/>
+    </div>
+  );
+}
+
+export default App;
+
+
