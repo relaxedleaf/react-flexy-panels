31b986817c6975b7be14caf789bfdffb6bc0d6a1
Guanghui Li <liguanghui.cs@gmail.com>
2025-12-04 22:35:30 -0600

    feat: expose onPreResize callback

---
+++ .changeset/funny-pianos-type.md
@@ -0,0 +1,5 @@
+---
+"react-flexy-panels": patch
+---
+
+feat: expose onPreResize prop on handle to allow any preprocess to be done before the panels are resized
--- packages/react-flexy-panels/src/components/FlexyPanelHandle/types/FlexyPanelHandleProps.t.ts
+++ packages/react-flexy-panels/src/components/FlexyPanelHandle/types/FlexyPanelHandleProps.t.ts
@@ -1,3 +1,6 @@
 import { ComponentProps } from "react";
+import { OnPreResizeFunction } from "../../../types";
 
-export type FlexyPanelHandleProps = ComponentProps<"div">;
+export type FlexyPanelHandleProps = ComponentProps<"div"> & {
+  onPreResize?: OnPreResizeFunction;
+};
